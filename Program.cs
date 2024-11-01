b84e00640515f9e4246cbee87654f5daf93cb2a2
julioarp2004@gmail.com <e107ti@etec.sp.gov.br>
2024-11-01 20:39:04 -0300

    Exercício SUB ROTINAS finalizadooo!!!

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿ExibirTitulo();
+Console.Write("Informe um número: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+bool ehPar = DefinirPar(numero);
+
+ExibirResultado(numero, ehPar);
+
+void ExibirTitulo()
+{
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.WriteLine("--------------------");
+    Console.WriteLine("--- Par ou Ímpar ---");
+    Console.WriteLine("-------------------\n");
+    Console.ResetColor();
+}
+
+bool DefinirPar(int numero)
+{
+    bool ehPar = numero % 2 == 0;
+    return ehPar;
+}
+
+void ExibirResultado(int numero, bool ehPar)
+{
+    Console.WriteLine($"O número {numero} é {(ehPar ? "Par" : "Ímpar")}");
+}
